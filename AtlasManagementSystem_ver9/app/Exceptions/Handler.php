12763a395130906a2adc26f615d974572805fa11
KOH\matsu <matsuda0509yukihiro@gmail.com>
2025-05-06 18:02:57 +0900

    セッションタイムアウト

--- AtlasManagementSystem_ver9/app/Exceptions/Handler.php
+++ AtlasManagementSystem_ver9/app/Exceptions/Handler.php
@@ -4,52 +4,43 @@ namespace App\Exceptions;
 
 use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Session\TokenMismatchException;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
     protected $dontReport = [
         //
     ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
     protected $dontFlash = [
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     *
-     * @throws \Exception
-     */
     public function report(Throwable $exception)
     {
         parent::report($exception);
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Exception
-     */
     public function render($request, Throwable $exception)
     {
+        // ▼ TokenMismatchException（419）をログイン画面へリダイレクト
+        if ($exception instanceof TokenMismatchException) {
+            return redirect()
+                ->route('loginView')
+                ->with('message', 'セッションの有効期限が切れました。もう一度ログインしてください。');
+        }
+
         return parent::render($request, $exception);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['message' => $exception->getMessage()], 401);
+        }
+
+        return redirect()->route('loginView');
     }
 }
