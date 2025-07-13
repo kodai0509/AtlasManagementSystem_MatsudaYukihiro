6dc61a60434441462d6669a9b61a4060f900d17a
KOH\matsu <matsuda0509yukihiro@gmail.com>
2025-07-13 17:47:55 +0900

    予約人数の表示、スクール予約詳細画面のリンク設置

--- AtlasManagementSystem_ver9/app/Calendars/Admin/CalendarWeekDay.php
+++ AtlasManagementSystem_ver9/app/Calendars/Admin/CalendarWeekDay.php
@@ -1,80 +1,99 @@
 <?php
+
 namespace App\Calendars\Admin;
 
 use Carbon\Carbon;
 use App\Models\Calendars\ReserveSettings;
 
-class CalendarWeekDay{
+class CalendarWeekDay
+{
   protected $carbon;
 
-  function __construct($date){
+  function __construct($date)
+  {
     $this->carbon = new Carbon($date);
   }
 
-  function getClassName(){
+  function getClassName()
+  {
     return "day-" . strtolower($this->carbon->format("D"));
   }
 
-  function render(){
+  function render()
+  {
     return '<p class="day">' . $this->carbon->format("j") . '日</p>';
   }
 
-  function everyDay(){
+  function everyDay()
+  {
     return $this->carbon->format("Y-m-d");
   }
 
-  function dayPartCounts($ymd){
+  function dayPartCounts($ymd)
+  {
     $html = [];
     $one_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '1')->first();
     $two_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '2')->first();
     $three_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '3')->first();
 
     $html[] = '<div class="text-left">';
-    if($one_part){
-      $html[] = '<p class="day_part m-0 pt-1">1部</p>';
+
+    // 予約人数を表示
+    if ($one_part) {
+      $one_part_reserved = $one_part->users ? $one_part->users->count() : 0;
+      $html[] = '<p class="day_part m-0 pt-1"><a href="' . route('calendar.admin.detail', ['date' => $ymd, 'part' => '1']) . '" class="text-decoration-none">1部</a>&nbsp;&nbsp;' . $one_part_reserved . '</p>';
     }
-    if($two_part){
-      $html[] = '<p class="day_part m-0 pt-1">2部</p>';
+
+    if ($two_part) {
+      $two_part_reserved = $two_part->users ? $two_part->users->count() : 0;
+      $html[] = '<p class="day_part m-0 pt-1"><a href="' . route('calendar.admin.detail', ['date' => $ymd, 'part' => '2']) . '" class="text-decoration-none">2部</a>&nbsp;&nbsp;' . $two_part_reserved . '</p>';
     }
-    if($three_part){
-      $html[] = '<p class="day_part m-0 pt-1">3部</p>';
+
+    if ($three_part) {
+      $three_part_reserved = $three_part->users ? $three_part->users->count() : 0;
+      $html[] = '<p class="day_part m-0 pt-1"><a href="' . route('calendar.admin.detail', ['date' => $ymd, 'part' => '3']) . '" class="text-decoration-none">3部</a>&nbsp;&nbsp;' . $three_part_reserved . '</p>';
     }
+
     $html[] = '</div>';
 
     return implode("", $html);
   }
 
-
-  function onePartFrame($day){
+  function onePartFrame($day)
+  {
     $one_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '1')->first();
-    if($one_part_frame){
+    if ($one_part_frame) {
       $one_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '1')->first()->limit_users;
-    }else{
+    } else {
       $one_part_frame = "20";
     }
     return $one_part_frame;
   }
-  function twoPartFrame($day){
+
+  function twoPartFrame($day)
+  {
     $two_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '2')->first();
-    if($two_part_frame){
+    if ($two_part_frame) {
       $two_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '2')->first()->limit_users;
-    }else{
+    } else {
       $two_part_frame = "20";
     }
     return $two_part_frame;
   }
-  function threePartFrame($day){
+
+  function threePartFrame($day)
+  {
     $three_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '3')->first();
-    if($three_part_frame){
+    if ($three_part_frame) {
       $three_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '3')->first()->limit_users;
-    }else{
+    } else {
       $three_part_frame = "20";
     }
     return $three_part_frame;
   }
 
-  //
-  function dayNumberAdjustment(){
+  function dayNumberAdjustment()
+  {
     $html = [];
     $html[] = '<div class="adjust-area">';
     $html[] = '<p class="d-flex m-0 p-0">1部<input class="w-25" style="height:20px;" name="1" type="text" form="reserveSetting"></p>';
